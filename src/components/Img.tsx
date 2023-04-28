3002818763d50e4313816a47000f5fb4d2332ebb
maddsua <102395937+maddsua@users.noreply.github.com>
2023-04-29 00:38:40 +0300

    More composition

--- src/components/Img.tsx
+++ src/components/Img.tsx
@@ -1,4 +1,21 @@
-import { decideSize } from "./util.js";
+interface i_sizes {
+	width: number | undefined,
+	height: number | undefined
+}
+
+export const decideSize = (sizes: number | number[] | undefined): i_sizes => {
+
+	if (!sizes) return { width: undefined, height: undefined }
+
+	if (typeof sizes === 'number') sizes = [sizes];
+
+	return {
+		//	@ts-ignore
+		width: sizes?.length >= 1 ? sizes[0] : undefined,
+		//	@ts-ignore
+		height: sizes?.length >= 2 ? sizes[1] : (sizes?.length == 1 ? sizes[0] : undefined)
+	}
+};
 
 interface Props {
 	src: string;
@@ -6,11 +23,12 @@ interface Props {
 	classlist?: string;
 	lazy?: boolean;
 	sizes?: number | number[];
+	draggable?: boolean;
 }
 
-export default ({src, alt, classlist, lazy, sizes}: Props) => {
+export default ({src, alt, classlist, lazy, sizes, draggable}: Props) => {
 
 	const size = decideSize(sizes);
 
-	return <img className={classlist} alt={alt} width={size.width} height={size.height} src={src} loading={ lazy !== false ? 'lazy' : undefined } draggable="false" />
+	return <img className={classlist} src={src} alt={alt} width={size.width} height={size.height} draggable={ draggable === true ? 'true' : 'false' } loading={ lazy !== false ? 'lazy' : undefined } />
 }
--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -1,4 +1,4 @@
-import { decideSize } from "./util.js";
+import Img from "./Img.jsx";
 
 interface Props {
 	src: string;
@@ -12,14 +12,12 @@ interface Props {
 
 export default ({src, alt, classlist, lazy, sizes, formats, draggable}: Props) => {
 
-	const size = decideSize(sizes);
-
 	return (
 		<picture className={classlist}>
 			{ ((typeof formats === 'string') ? formats.replace('\s','').split(',') : formats)?.map((format, index) => (
 				<source key={`src_${index}`} srcSet={ (src.replace(/\.[\w\d]+/, '.') + format) } type={ `image/${format}` } />
 			)) }
-			<img src={src} alt={alt} draggable={ draggable === true ? 'true' : 'false' } loading={ lazy !== false ? 'lazy' : undefined } width={size.width} height={size.height} />
+			<Img src={src} alt={alt} draggable={draggable} lazy={lazy} sizes={sizes} />
 		</picture>
 	);
 }
