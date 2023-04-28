87f0135fc350942df7ec6834dd0690d024889001
maddsua <102395937+maddsua@users.noreply.github.com>
2023-04-29 00:21:47 +0300

    finally reacts seems to be happy

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -16,8 +16,8 @@ export default ({src, alt, classlist, lazy, sizes, formats, draggable}: Props) =
 
 	return (
 		<picture className={classlist}>
-			{ ((typeof formats === 'string') ? formats.replace('\s','').split(',') : formats)?.map((format: string) => (
-				<source srcSet={ (src.replace(/\.[\w\d]+/, '.') + format) } type={ `image/${format}` } />
+			{ ((typeof formats === 'string') ? formats.replace('\s','').split(',') : formats)?.map((format, index) => (
+				<source key={`src_${index}`} srcSet={ (src.replace(/\.[\w\d]+/, '.') + format) } type={ `image/${format}` } />
 			)) }
 			<img src={src} alt={alt} draggable={ draggable === true ? 'true' : 'false' } loading={ lazy !== false ? 'lazy' : undefined } width={size.width} height={size.height} />
 		</picture>
