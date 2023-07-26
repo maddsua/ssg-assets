48725e2f3e6c378cd2cf288dff009d4efe9fd1a6
maddsua <maddsuadev@proton.me>
2023-07-26 11:15:35 +0300

    Update Picture.vue

--- components/vue/Picture.vue
+++ components/vue/Picture.vue
@@ -4,14 +4,14 @@ import Img from './Img.vue';
 import { Props, mapSources } from '../components_shared';
 
 const { src, alt, classlist, lazy, sizes, formats, draggable, adaptiveModes } = defineProps<Props>();
-const useSources = mapSources(src, formats, adaptiveModes);
+const sources = mapSources(src, formats, adaptiveModes);
 
 </script>
 
 <template>
 
 	<picture :class="classlist" data-maddsua-component="vue:ssgassets:picture">
-		<source v-for="item of useSources" :srcset="item.source" :type="item.type" :media="(item.media as string | undefined)" />
+		<source v-for="item of sources" :srcset="item.source" :type="item.type" :media="(item.media as string | undefined)" />
 		<Img :src="src" :alt="alt" :draggable="draggable" :lazy="lazy" :sizes="sizes" />
 	</picture>
 
