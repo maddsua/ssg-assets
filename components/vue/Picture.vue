85222e8eaf3cc3124fc4da3eb456eb0d6e98d773
maddsua <maddsuadev@proton.me>
2023-07-26 10:49:28 +0300

    Update Picture.vue

--- components/vue/Picture.vue
+++ components/vue/Picture.vue
@@ -2,6 +2,15 @@
 
 import Img from './Img.vue';
 
+/**
+ * Supported image formats are: avif, webp, png, jpg
+ */
+
+interface AdaptiveMode {
+	media: string;
+	modifier: string;
+}
+
 interface Props {
 	src: string;
 	alt: string;
@@ -10,17 +19,35 @@ interface Props {
 	draggable?: boolean;
 	lazy?: boolean;
 	sizes?: number | number[];
+	adaptiveModes?: AdaptiveMode[];
 }
-const { src, alt, classlist, lazy, sizes, formats, draggable } = defineProps<Props>();
+const { src, alt, classlist, lazy, sizes, formats, draggable, adaptiveModes } = defineProps<Props>();
 
-const sourcesList = formats ? (typeof formats === 'string') ? formats.replace('\s','').split(',') : formats : [];
+const supportedFormats = [ 'avif', 'webp', 'png', 'jpg' ];
+const requestedFormats = formats ? (typeof formats === 'string') ? formats.replace('\s','').split(',') : formats : [];
+const imageAltFormats = supportedFormats.filter(format => requestedFormats.some(item => item.toLowerCase() === format));
+
+const sources = imageAltFormats.map(format => ({
+	source: `${src.replace(/\.[\w\d]+/, '')}.${format}`,
+	type: `image/${format}`,
+	media: null
+}));
+
+const adaptiveSources = adaptiveModes.map(sourceMedia => sources.map(sourceFormats => {
+	const { source } = sourceFormats;
+	sourceFormats.media = sourceMedia.media;
+	sourceFormats.source = source.replace(/\..*$/, '') + sourceMedia.modifier + source.match(/\..*$/)?.[0];
+	return sourceFormats;
+})).flat(2);
+
+const useSources = adaptiveSources.length ? adaptiveSources : sources;
 
 </script>
 
 <template>
 
 	<picture :class="classlist" data-maddsua-component="vue:ssgassets:picture">
-		<source v-for="format of sourcesList" :srcset="src.replace(/\.[\w\d]+/, '.') + format" :type="`image/${format}`" />
+		<source v-for="item of useSources" :srcset="item.source" :type="item.type" />
 		<Img :src="src" :alt="alt" :draggable="draggable" :lazy="lazy" :sizes="sizes" />
 	</picture>
 
