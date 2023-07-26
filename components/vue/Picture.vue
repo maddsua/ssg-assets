831fc4ad7e97f6bc7c56be8d3440b54611ed8d07
maddsua <maddsuadev@proton.me>
2023-07-26 10:59:10 +0300

    Update Picture.vue

--- components/vue/Picture.vue
+++ components/vue/Picture.vue
@@ -33,21 +33,21 @@ const sources = imageAltFormats.map(format => ({
 	media: null
 }));
 
-const adaptiveSources = adaptiveModes.map(sourceMedia => sources.map(sourceFormats => {
+const adaptiveSources = adaptiveModes?.map(sourceMedia => sources.map(sourceFormats => {
 	const { source } = sourceFormats;
 	sourceFormats.media = sourceMedia.media;
 	sourceFormats.source = source.replace(/\..*$/, '') + sourceMedia.modifier + source.match(/\..*$/)?.[0];
 	return sourceFormats;
 })).flat(2);
 
-const useSources = adaptiveSources.length ? adaptiveSources : sources;
+const useSources = adaptiveSources?.length ? adaptiveSources : sources;
 
 </script>
 
 <template>
 
 	<picture :class="classlist" data-maddsua-component="vue:ssgassets:picture">
-		<source v-for="item of useSources" :srcset="item.source" :type="item.type" />
+		<source v-for="item of useSources" :srcset="item.source" :type="item.type" :media="`(${item.media})`" />
 		<Img :src="src" :alt="alt" :draggable="draggable" :lazy="lazy" :sizes="sizes" />
 	</picture>
 
