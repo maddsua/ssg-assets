8512218ff3116a42601c42edaa5931eb369fae61
maddsua <maddsuadev@proton.me>
2023-07-26 14:21:01 +0300

    Fixed picture sources reactivity for Vue

--- components/vue/Picture.vue
+++ components/vue/Picture.vue
@@ -4,14 +4,13 @@ import Img from './Img.vue';
 import { Props, mapSources } from '../components_shared';
 
 const { src, alt, classlist, lazy, sizes, formats, draggable, adaptiveModes } = defineProps<Props>();
-const sources = mapSources(src, formats, adaptiveModes);
 
 </script>
 
 <template>
 
 	<picture :class="classlist" data-maddsua-component="vue:ssgassets:picture">
-		<source v-for="item of sources" :srcset="item.source" :type="item.type" :media="(item.media as string | undefined)" />
+		<source v-for="item of mapSources(src, formats, adaptiveModes)" :srcset="item.source" :type="item.type" :media="(item.media as string | undefined)" />
 		<Img :src="src" :alt="alt" :draggable="draggable" :lazy="lazy" :sizes="sizes" />
 	</picture>
 
