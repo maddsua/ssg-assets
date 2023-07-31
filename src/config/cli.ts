d64d37c9e6d87405828d24023ba29474fc75344d
maddsua <maddsuadev@proton.me>
2023-07-31 14:23:10 +0300

    Create cli.ts

---
+++ src/config/cli.ts
@@ -0,0 +1,40 @@
+export interface Argument {
+	pfx: string[];
+	actions: Array< 'get_value' | 'impl_bool' | 'to_string_array' >;
+}
+
+export const cliArguments: Record<string, Argument> = {
+	config: {
+		pfx: ['--config'],
+		actions: ['get_value']
+	},
+	verbose: {
+		pfx: ['--verbose'],
+		actions: ['impl_bool']
+	},
+	nocache: {
+		pfx: ['--nocache'],
+		actions: ['impl_bool']
+	},
+	justCopy: {
+		pfx: ['--copy'],
+		actions: ['impl_bool']
+	},
+	formats: {
+		pfx: ['--formats'],
+		actions: ['get_value', 'to_string_array']
+	},
+	exclude: {
+		pfx: ['--exclude'],
+		actions: ['get_value', 'to_string_array']
+	},
+	inputDir: {
+		pfx: ['--input'],
+		actions: ['get_value']
+	},
+	outputDir: {
+		pfx: ['--output'],
+		actions: ['get_value']
+	},
+
+};
