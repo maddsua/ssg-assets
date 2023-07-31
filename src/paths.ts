56cdc13727ce19ad09017c00b16904617d723a38
maddsua <maddsuadev@proton.me>
2023-07-31 15:59:21 +0300

    Update paths.ts

--- src/paths.ts
+++ src/paths.ts
@@ -1,1 +1,24 @@
+import { existsSync, statSync } from 'fs';
+
 export const normalizePath = (filepath: string) => filepath.replace(/[\\\/]+/g, '/');
+
+export const fix_relative_glob = (pattern: string) => {
+
+	if (/\*\.[\d\w]+$/.test(pattern)) {
+
+		return '**/' + pattern.slice(pattern.lastIndexOf('*'));
+
+	} else if (!pattern.includes('*')) {
+
+		try {
+			if (!existsSync(pattern)) return pattern;
+			if (!statSync(pattern).isDirectory()) return pattern;
+		} catch (_error) {
+			return pattern;
+		}
+	
+		return pattern + ( pattern.endsWith('/') ? '*': '/*');
+	}
+
+	return pattern;
+};
