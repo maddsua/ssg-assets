059c617d22243cad844984b44c2acb489ac4e4d1
maddsua <maddsuadev@proton.me>
2023-07-31 14:54:45 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
-import { loadConfig } from "./config/loader";
-
-const config = loadConfig();
-
-console.log(config);
-
 import path from "path";
 import fs from 'fs';
-import { createHash } from "crypto";
+import { createHash } from 'crypto';
 
 import sharp from 'sharp';
 import chalk from 'chalk';
 import { globSync } from 'glob';
 import { minimatch } from 'minimatch';
+
+import { loadConfig } from "./config/loader";
+
+const config = loadConfig();
+
+if (config.verbose) {
+	console.log('Verbose mode enabled. The tool is extra talkative now.');
+	console.log('Current config:', config);
+}
