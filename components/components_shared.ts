511c5c30f7b03e5213ff6fc5e4a7fa76b78cbbb2
maddsua <maddsuadev@proton.me>
2023-08-11 21:43:57 +0300

    Update components_shared.ts
    Update components_shared.ts
    
    Update components_shared.ts

--- components/components_shared.ts
+++ components/components_shared.ts
@@ -1,15 +1,15 @@
-import { Prop } from "vue";
-
 export interface AdaptiveMode {
 	media: string;
 	modifier: string | null | undefined;
 }
 
+type ImageFormats = string | string[];
+
 export interface Props {
 	src: string;
 	alt: string;
 	classlist?: string;
-	formats?: string | string[];
+	formats?: ImageFormats;
 	draggable?: boolean;
 	lazy?: boolean;
 	sizes?: number | number[];
@@ -18,10 +18,10 @@ export interface Props {
 
 export const supportedFormats = [ 'avif', 'webp', 'png', 'jpg' ];
 
-export const mapSources = (src: Props['src'], formats: Props['formats'], adaptiveModes: Props['adaptiveModes']) => {
+export const mapSources = (src: string, formats?: ImageFormats, adaptiveModes?: AdaptiveMode[]) => {
 
 	const requestedFormats = formats ? (typeof formats === 'string') ? formats.replace('\s','').split(',') : formats : [];
-	const imageAltFormats = supportedFormats.filter(format => requestedFormats.some(item => item.toLowerCase() === format));
+	const imageAltFormats = requestedFormats.filter(format => supportedFormats.some(item => item.toLowerCase() === format));
 	
 	const sources = imageAltFormats.map(format => ({
 		source: `${src.replace(/\.[\w\d]+/, '')}.${format}`,
@@ -29,9 +29,9 @@ export const mapSources = (src: Props['src'], formats: Props['formats'], adaptiv
 		media: null as string | null
 	}));
 	
-	const adaptiveSources = adaptiveModes?.map(i_media => sources.map(i_src => ({
-		media: `(${i_media.media})`,
-		source: i_media.modifier ? i_src.source.replace(/\..*$/, '') + i_media.modifier + i_src.source.match(/\..*$/)?.[0] : i_src.source,
+	const adaptiveSources = adaptiveModes?.map(item => sources.map(i_src => ({
+		media: `(${item.media})`,
+		source: item.modifier ? i_src.source.replace(/\..*$/, '') + item.modifier + i_src.source.match(/\..*$/)?.[0] : i_src.source,
 		type: i_src.type
 	}))).flat(1);
 	
