ff7bbde9fa274ea259531f050138782800a6c0ff
maddsua <maddsuadev@proton.me>
2023-08-25 13:30:08 +0300

    Update components_shared.ts

--- components/components_shared.ts
+++ components/components_shared.ts
@@ -1,7 +1,9 @@
+
+type ModeModifier = string | null | undefined;
 export interface AdaptiveMode {
 	media: string;
-	modifier: string | null | undefined;
-}
+	modifier: ModeModifier;
+};
 
 type ImageFormats = string | string[];
 
@@ -27,6 +29,13 @@ export interface ImageProps {
 
 export const supportedFormats = [ 'avif', 'webp', 'png', 'jpg' ];
 
+const applyUrlModifier = (src: string, modifier: ModeModifier) => modifier ? src.replace(/\..*$/, '') + modifier + src.match(/\..*$/)?.[0] : src;
+
+export const adaptBaseImageUrl = (src: string, adaptiveModes?: AdaptiveMode[]) => {
+	if (!adaptiveModes || adaptiveModes?.length < 2) return src;
+	return applyUrlModifier(src, adaptiveModes[0].modifier);
+};
+
 export const mapSources = (src: string, formats?: ImageFormats, adaptiveModes?: AdaptiveMode[]) => {
 
 	const requestedFormats = formats ? (typeof formats === 'string') ? formats.replace('\s','').split(',') : formats : [];
@@ -38,11 +47,11 @@ export const mapSources = (src: string, formats?: ImageFormats, adaptiveModes?: 
 		media: null as string | null
 	}));
 	
-	const adaptiveSources = adaptiveModes?.map(item => sources.map(i_src => ({
-		media: `(${item.media})`,
-		source: item.modifier ? i_src.source.replace(/\..*$/, '') + item.modifier + i_src.source.match(/\..*$/)?.[0] : i_src.source,
-		type: i_src.type
+	const adaptiveSources = adaptiveModes?.map(mode => sources.map(item => ({
+		media: `(${mode.media})`,
+		source: mode.modifier ? item.source.replace(/\..*$/, '') + mode.modifier + item.source.match(/\..*$/)?.[0] : item.source,
+		type: item.type
 	}))).flat(1);
 	
 	return adaptiveSources?.length ? adaptiveSources : sources || [];
-}
+};
