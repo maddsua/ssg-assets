8110b41e8eb86ed274b3b2701291abe4b11725ae
maddsua <maddsuadev@proton.me>
2023-08-27 16:13:58 +0300

    Update lazyload.ts

--- components/common/lazyload.ts
+++ components/common/lazyload.ts
@@ -5,18 +5,19 @@ export default (root?: HTMLElement | Element | null, componentID?: string) => {
 	const lazyImages = (root || document).querySelectorAll<HTMLImageElement>('img[loading="lazy"]');
 	const attachToImages = componentID ? Array.from(lazyImages).filter(item => item.getAttribute('data-component-id') === componentID) : lazyImages;
 
+
 	attachToImages.forEach(image => {
 
 		if (image.complete) return;
 
-		image.style.transition = `opacity 500ms ease`;
 		image.style.opacity = '0';
-
+		
 		image.onload = async () => {
-
-			await asyncSleep(10);
+			
+			image.style.transition = `opacity 500ms ease`;
+			await asyncSleep(100);
 			image.style.opacity = '1';
-
+			
 			await asyncSleep(500);
 			image.style.transition = '';
 			image.style.opacity = '';
