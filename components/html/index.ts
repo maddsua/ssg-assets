a685977771d805a0aa7023538ceaac16eb1651a7
maddsua <maddsuadev@proton.me>
2023-08-27 15:43:25 +0300

    Update index.ts

--- components/html/index.ts
+++ components/html/index.ts
@@ -1,7 +1,11 @@
 import Picture from './Picture';
 import Img from './Img';
+import lazyload from '../common/lazyload';
+
+const enableLazyLoad = (root?: HTMLElement | Element) => lazyload(root, 'ssga:img:html');
 
 export {
 	Picture,
-	Img
+	Img,
+	enableLazyLoad
 };
