6a1a0b9403368d34e0327c846bdfc25ed4f76c0e
maddsua <maddsuadev@proton.me>
2023-08-27 15:34:47 +0300

    Update Picture.vue

--- components/vue/Picture.vue
+++ components/vue/Picture.vue
@@ -9,7 +9,7 @@ const props = defineProps<PictireProps>();
 
 <template>
 
-	<picture :class="props.class" :style="styleToString(props.style)" data-maddsua-component="vue:ssgassets:picture">
+	<picture :class="props.class" :style="styleToString(props.style)" data-component-id="ssga:picture:vue">
 		<source v-for="item of mapSources(props.src, props.formats, props.adaptiveModes)" :srcset="item.source" :type="item.type" :media="(item.media as string | undefined)" />
 		<Img :src="adaptBaseImageUrl(props.src, props.adaptiveModes)" :alt="props.alt" :draggable="props.draggable" :lazy="props.lazy" :sizes="props.sizes" :class="props.imgClass" :style="props.imgStyle" />
 	</picture>
