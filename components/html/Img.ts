97ffff4a9b322e10f4b3fd8278136f8f7a17d975
maddsua <maddsuadev@proton.me>
2023-08-27 14:43:31 +0300

    Create Img.ts

---
+++ components/html/Img.ts
@@ -0,0 +1,19 @@
+import { ImageProps, getImageSize, classToString, styleToString, attributeListToString } from '../components_shared';
+
+export default (props: ImageProps) => {
+
+	const size = getImageSize(props.sizes);
+
+	const attrList: [string, any][] = [
+		['src', props.src],
+		['alt', props.alt],
+		['width', size?.width],
+		['height', size?.width],
+		['draggable', props.draggable],
+		['loading', props.lazy !== false ? 'lazy' : undefined],
+		['class', classToString(props.class)],
+		['style', styleToString(props.style)]
+	];
+
+	return `<img ${attributeListToString(attrList)}/>`;
+};
