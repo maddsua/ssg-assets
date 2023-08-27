a7d51e4b4aef7199fd629a1f32aa050c1b69ab21
maddsua <maddsuadev@proton.me>
2023-08-27 15:47:32 +0300

    Update html.d.ts

--- html.d.ts
+++ html.d.ts
@@ -1,7 +1,10 @@
 import Picture from './components/html/Picture';
 import Img from './components/html/Img';
 
+declare const enableLazyLoad: (root?: HTMLElement | Element) => void;
+
 export {
 	Picture,
-	Img
+	Img,
+	enableLazyLoad
 }
