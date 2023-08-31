c32e80277eef7f50dcd9608e5e86d64ab13cdfbb
maddsua <maddsuadev@proton.me>
2023-08-31 04:13:35 +0300

    Update index.ts

--- components/react/index.ts
+++ components/react/index.ts
@@ -1,11 +1,9 @@
 import Picture from './Picture';
 import Img from './Img';
-import { revealLazyLoaded} from '../index';
-
-const enableLazyLoad = (root?: HTMLElement | Element) => revealLazyLoaded(root, 'ssga:img:react');
+import { revealLazyLoaded } from '../index';
 
 export {
 	Picture,
 	Img,
-	enableLazyLoad
+	revealLazyLoaded
 };
