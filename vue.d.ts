1e99927db131c4f3e09b3b1006e29224d6a0a7f1
maddsua <maddsuadev@proton.me>
2023-08-31 04:22:00 +0300

    Fixed declarations

--- vue.d.ts
+++ vue.d.ts
@@ -1,14 +1,14 @@
 //	This file is needed to some versions of typescript to f off when modules resolution is eff'd too
 //	just ignore it's presense.
 
-import type { ImageProps, PictireProps } from './components/common/setup';
+import type { ImageProps, PictireProps } from './components/index';
+import { revealLazyLoaded } from './components/index';
 
 declare const Img: (props: ImageProps) => any;
 declare const Picture: (props: PictireProps) => any;
-declare const enableLazyLoad: (root?: HTMLElement | Element) => void;
 
 export {
 	Img,
 	Picture,
-	enableLazyLoad
+	revealLazyLoaded
 }
