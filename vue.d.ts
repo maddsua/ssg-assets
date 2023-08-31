03fb85346d7e95a0db576e9b179dc6d8a865e4b6
maddsua <maddsuadev@proton.me>
2023-08-31 04:23:13 +0300

    Update vue.d.ts

--- vue.d.ts
+++ vue.d.ts
@@ -1,8 +1,7 @@
 //	This file is needed to some versions of typescript to f off when modules resolution is eff'd too
 //	just ignore it's presense.
 
-import type { ImageProps, PictireProps } from './components/index';
-import { revealLazyLoaded } from './components/index';
+import { ImageProps, PictireProps, revealLazyLoaded } from './components/index';
 
 declare const Img: (props: ImageProps) => any;
 declare const Picture: (props: PictireProps) => any;
