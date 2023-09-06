47c9d3598771784fb2edb79efc4aec34ab4d524e
maddsua <maddsuadev@proton.me>
2023-09-06 23:10:48 +0300

    Update test.ts

--- tests/components/test.ts
+++ tests/components/test.ts
@@ -6,11 +6,51 @@
 
 import { mapSources, adaptBaseImageUrl } from '../../components/index.ts';
 
+const assertEqual = (a: any, b: any) => {
+	if (a === b) return;
+	console.error('\r\nAssert arguments did not match:', '\r\nArgument A:', a, '\r\nArgument B:', b, '\r\n');
+	throw new Error('Assertion failed');
+};
 
+
+/**
+ * Test 1
+ * Generate source context and transform image src for:
+ * 	Single adaptive mode
+ */
 (() => {
 
 	const input = {
 		url: '/cats/1.png',
+		adaptive: [
+			{
+				'media': 'orientation: portrait',
+				'modifier': '.mobile'
+			}
+		]
+	}
+
+	const outputSources = mapSources(input.url, undefined, input.adaptive);
+	const outputImageSrc = adaptBaseImageUrl(input.url, input.adaptive);
+
+	assertEqual(outputSources, null)
+
+	console.log(outputSources);
+	console.log(outputImageSrc);
+
+})();
+
+
+/**
+ * Test 2
+ * Generate source context and transform image src for:
+ * 	Single adaptive mode
+ * 	Single image format
+ */
+(() => {
+
+	const input = {
+		url: '/cats/2.png',
 		adaptive: [
 			{
 				'media': 'orientation: portrait',
@@ -23,8 +63,8 @@ import { mapSources, adaptBaseImageUrl } from '../../components/index.ts';
 	const outputSources = mapSources(input.url, input.formats, input.adaptive);
 	const outputImageSrc = adaptBaseImageUrl(input.url, input.adaptive);
 
-	console.log(outputSources)
-	console.log(outputImageSrc)
+	console.log(outputSources);
+	console.log(outputImageSrc);
 
 })();
 
