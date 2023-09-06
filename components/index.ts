43abc23db87bcc9a89d4d296a6e97bae74c4770e
maddsua <maddsuadev@proton.me>
2023-09-06 23:04:00 +0300

    Revert "Update index.ts"
    This reverts commit 1d517844ee76e19d4a380e8a44a6abc94ed83298.

--- components/index.ts
+++ components/index.ts
@@ -63,11 +63,13 @@ export const mapSources = (baseImageSrc: string, formats?: ImageFormatsType, ada
 
 	if (!adaptiveModes?.length) return altFormatSources;
 
-	return altFormatSources.length ? adaptiveModes.map(mode => altFormatSources.map(source => ({
+	const mapAdaptiveModes = adaptiveModes.length > 1 ? adaptiveModes : [{ media: null, modifier: adaptiveModes[0].modifier }];
+
+	return altFormatSources.length ? mapAdaptiveModes.map(mode => altFormatSources.map(source => ({
 		media: mode.media ? `(${mode.media})` : undefined,
 		source: applyUrlModifier(source.source, mode.modifier),
 		type: source.type
-	}))).flat(1) : adaptiveModes.map(mode => ({
+	}))).flat(1) : mapAdaptiveModes.map(mode => ({
 		media: mode.media ? `(${mode.media})` : undefined,
 		source: applyUrlModifier(baseImageSrc, mode.modifier),
 		type: `image/${baseImageSrc.replace(expressions.allBeforeExtension, '')}`
