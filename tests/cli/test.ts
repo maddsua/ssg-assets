8a5b1aeacf1de41c68238a3f7d05574566825af3
maddsua <maddsuadev@proton.me>
2023-09-14 01:45:49 +0300

    Changed cli internal test to aim towards correct asset processing

--- tests/cli/test.ts
+++ tests/cli/test.ts
@@ -1,56 +1,31 @@
 import { loadAppConfig } from '../../src/config/loader';
-import { minimatch } from 'minimatch';
-import { ConfigSchema } from '../../src/config/schema';
+
+import { resolveAssets } from '../../src/content/loader';
 
 let appConfig = loadAppConfig();
 console.log(appConfig);
 
+const assets = await resolveAssets(appConfig);
+console.log(assets);
+
 if (!appConfig.verbose) throw new Error('"verbose" option was not picked up from config file');
 
-const propsShouldMatch: Partial<ConfigSchema> = {
-	inputDir: 'tests/convert/assets',
-	outputDir: 'tests/convert/dist',
-	cacheDir: 'tests/convert/assets/.cache'
-};
-
-for (let item in propsShouldMatch) {
-	if (appConfig[item] !== propsShouldMatch[item])
-		throw new Error(`Option "${item}" does not match: expected "${propsShouldMatch[item]}, have "${appConfig[item]}"`)
-}
-
-const globExpressions = appConfig.exclude;
-const pathsToMatch = [
-	'some/vector/image.svg',
-	'some/ui/image.svg',
-	'some/cats/cute.jpg',
-	'places/paris/thatCODpicture.webp'
+const matchAssetAction = [
+	{
+		slug: 'images/azamat-esmurziyev-qhdGyb-jw2M-unsplash.jpg',
+		action: 'sharp'
+	},
+	{
+		slug: 'vector/bg.svg',
+		action: undefined
+	},
+	{
+		slug: 'ui/button.svg',
+		action: 'copy'
+	}
 ];
 
-const matchMatrics = pathsToMatch.map(filePath => {
-	const matches = globExpressions.map(pattern => ({
-		pattern,
-		matched: minimatch(filePath, pattern, {
-			matchBase: true,
-			nobrace: true,
-			noext: true,
-			nocase: true
-		})
-	}));
-
-	return {
-		path: filePath,
-		pattern: matches.find(item => item.matched)?.pattern
-	};
-
-}).filter(item => item.pattern);
-
-const matchPathsExpected = [
-	'some/vector/image.svg'
-];
-
-console.log({ matchMatrics, matchPathsExpected });
-
-const matchedPatterns = matchMatrics.map(item => item.path);
-
-if (matchedPatterns.some((item, index) => matchPathsExpected[index] !== item))
-	throw new Error(`Glob matches are differend`);
+matchAssetAction.forEach(item => {
+	const asset = assets.find(asset => asset.slug === item.slug);
+	if (asset?.action !== item.action) throw new Error(`Asset "${item.slug}" action expected to be "${item.action}" but is "${asset?.action}" instead`);
+});
