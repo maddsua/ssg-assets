a1960c248523b5e330e5401863f25729d70999fd
maddsua <maddsuadev@proton.me>
2023-11-19 07:50:17 +0200

    Update Img.vue

--- components/vue/Img.vue
+++ components/vue/Img.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import { type ImageProps, getImageSize, styleToString } from '../index';
+import { type ImageProps, getImageSize, styleToString, applyImageSrc } from '../index';
 
 const props = defineProps<ImageProps>();
 const size = getImageSize(props.sizes);
@@ -9,6 +9,6 @@ const size = getImageSize(props.sizes);
 
 <template>
 
-	<img :src="props.src" :alt="props.alt" :width="size?.width" :height="size?.height" :draggable="props.draggable === true" :loading="props.lazy !== false ? 'lazy' : undefined" :class="props.class" :style="styleToString(props.style)" data-ssga-id="img:vue" />
+	<img :src="applyImageSrc(props.src)" :alt="props.alt" :width="size?.width" :height="size?.height" :draggable="props.draggable === true" :loading="props.lazy !== false ? 'lazy' : undefined" :class="props.class" :style="styleToString(props.style)" data-ssga-id="img:vue" />
 
 </template>
