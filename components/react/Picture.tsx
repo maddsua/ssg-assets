3b100cc6e21f7f2b3bcfa40f9bb300dcf557c590
maddsua <maddsuadev@proton.me>
2023-11-19 07:50:14 +0200

    Update Picture.tsx

--- components/react/Picture.tsx
+++ components/react/Picture.tsx
@@ -13,7 +13,14 @@ export default (props: PictireProps) => {
 			{ sources.map(item => (
 				<source srcset={item.source} type={item.type} media={item.media as string | undefined} />
 			)) }
-			<Img src={props.src} alt={props.alt} draggable={props.draggable} lazy={props.lazy} sizes={props.sizes} class={props.imgClass} style={props.imgStyle} />
+			<Img src={props.src}
+				alt={props.alt}
+				draggable={props.draggable}
+				lazy={props.lazy}
+				sizes={props.sizes}
+				class={props.imgClass}
+				style={props.imgStyle}
+			/>
 		</picture>
 	);
 };
