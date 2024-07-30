de685111c72d502be122884a365f7a0e3fa44628
maddsua <maddsuadev@proton.me>
2024-07-31 02:20:19 +0300

    made test more readable

--- tests/predeploy.test.ts
+++ tests/predeploy.test.ts
@@ -6,7 +6,8 @@ Deno.test('npm package integrity', async () => {
 	})).default;
 
 	//	check that all ui component submodules are exported and accessible
-	const componentSubmodules = Array.from(Deno.readDirSync(Deno.cwd().endsWith('tests') ? '../components' : './components')).filter(item => item.isDirectory).map(item => item.name);
+	const componentSubmodules = Array.from(Deno.readDirSync(Deno.cwd().endsWith('tests') ? '../components' : './components'))
+		.filter(item => item.isDirectory).map(item => item.name);
 
 	const submoduleExportRecords = componentSubmodules.map(item => ({
 		name: item,
@@ -15,23 +16,45 @@ Deno.test('npm package integrity', async () => {
 
 	//	package exports
 	const uiComponentsNotExported = submoduleExportRecords.filter(item => !item.record).map(item => item.name);
-	if (uiComponentsNotExported.length) throw new Error(`Submodules not exported from package.json: ${uiComponentsNotExported.join(', ')}`);
+	if (uiComponentsNotExported.length) {
+		throw new Error(`Submodules not exported from package.json: ${uiComponentsNotExported.join(', ')}`);
+	}
 
 	//	package default export
-	const noDefaultExport = submoduleExportRecords.filter(item => item.record.default !== `./components/${item.name}/index.ts`).map(item => item.name);
-	if (noDefaultExport.length) throw new Error(`Submodules do not have default export option in package.json: ${noDefaultExport.join(', ')}`);
+	const noDefaultExport = submoduleExportRecords
+		.filter(item => item.record.default !== `./components/${item.name}/index.ts`)
+		.map(item => item.name);
+
+	if (noDefaultExport.length) {
+		throw new Error(`Submodules do not have default export option in package.json: ${noDefaultExport.join(', ')}`);
+	}
 
 	//	package types export
-	const noTypesExport = submoduleExportRecords.filter(item => item.record.types !== `./components/${item.name}/index.ts`).map(item => item.name);
-	if (noTypesExport.length) throw new Error(`Submodules do not have types export option in package.json: ${noTypesExport.join(', ')}`);
+	const noTypesExport = submoduleExportRecords
+		.filter(item => item.record.types !== `./components/${item.name}/index.ts`).
+		map(item => item.name);
+
+	if (noTypesExport.length) {
+		throw new Error(`Submodules do not have types export option in package.json: ${noTypesExport.join(', ')}`);
+	}
 
 	//	check that d.ts is available for all ui components
-	const dtss = Array.from(Deno.readDirSync(Deno.cwd().endsWith('tests') ? '../' : './')).filter(item => item.isFile && item.name.endsWith('.d.ts')).map(item => item.name);
-	const submodulesWithNoDTS = componentSubmodules.filter(submod => !dtss.some(dts => dts === `${submod}.d.ts`));
-	if (submodulesWithNoDTS.length) throw new Error(`Submodules do not have a fallback .d.ts declaration: ${submodulesWithNoDTS.map(item => item).join(', ')}`);
+	const dtsFilesList = Array.from(Deno.readDirSync(Deno.cwd().endsWith('tests') ? '../' : './'))
+		.filter(item => item.isFile && item.name.endsWith('.d.ts'))
+		.map(item => item.name);
+	
+	const dtsSet = new Set<string>(dtsFilesList);
+
+	const submodulesWithNoDTS = componentSubmodules.filter(submod => !dtsSet.has(`${submod}.d.ts`));
+	if (submodulesWithNoDTS.length) {
+		throw new Error(`Submodules do not have a fallback .d.ts declaration: ${submodulesWithNoDTS.map(item => item).join(', ')}`);
+	}
 	
 	//	check that each ui component directory is added to package.json files field
-	const filesToBePacked = packageinfo.files;
-	const submodulesNotIncluded = componentSubmodules.filter(submod => !filesToBePacked.some(file => file === `components/${submod}`));
-	if (submodulesNotIncluded.length) throw new Error(`Submodules is not exported in package.json/files: ${submodulesNotIncluded.map(item => item).join(', ')}`);
+	const filesToBePacked = new Set<string>(packageinfo.files);
+	const submodulesNotIncluded = componentSubmodules.filter(submod => !filesToBePacked.has(`components/${submod}`));
+
+	if (submodulesNotIncluded.length) {
+		throw new Error(`Submodules is not exported in package.json/files: ${submodulesNotIncluded.map(item => item).join(', ')}`);
+	}
 });
